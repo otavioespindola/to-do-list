5dc03ce71eedebbb120d08cbbc3f8d0e6aa5d6be
Otavio Espindola <otavioespindola@gmail.com>
2022-02-21 16:57:37 -0300

    testantos rotas get, post, put e delet com postman

---
+++ src/routes/checklist.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    console.log("Olá");
+    res.send();
+})
+
+router.post('/', (req,res) => {
+    console.log(req.body);
+    res.status(200).json(req.body);
+})
+
+router.get("/:id", (req,res) => {
+    console.log(req.params)
+    res.send(`ID : ${req.params.id}`)
+})
+
+router.put('/:id', (req,res) => {
+    res.send(`PUT ID : ${req.params.id}`)
+})
+
+router.delete('/:id', (req,res) => {
+    res.send(`Delete ID : ${req.params.id}`)
+})
+
+module.exports = router;
