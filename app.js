b71601742fb6529bce9268755b4f53f76f477ea9
Otavio Espindola <otavioespindola@gmail.com>
2022-02-23 11:23:37 -0300

    instalado módulo EJS e definida rota para view

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const checkListRouter = require("./src/routes/checklist")
-require('./config/database')
+const path = require('path');
+
+const checkListRouter = require("./src/routes/checklist");
+const rootRouter = require('./src/routes/index');
+
+require('./config/database');
 
 const app = express();
 app.use(express.json());
 
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
+
+app.use("/" ,rootRouter);
 app.use("/checklists" ,checkListRouter);
 
 app.listen(3000, ()=> {
