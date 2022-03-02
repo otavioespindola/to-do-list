0c5199f6b29ffa84244f871d3abfd6351cce8ae2
Otavio Espindola <otavioespindola@gmail.com>
2022-03-02 11:29:09 -0300

    Implementada função de adicionar nova tarefa

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const path = require('path');
 
 const checkListRouter = require("./src/routes/checklist");
 const rootRouter = require('./src/routes/index');
+const methodOverride = require('method-override');
 
 require('./config/database');
 
 const app = express();
 app.use(express.json());
-
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(methodOverride('_method'));  
+
 
 app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
